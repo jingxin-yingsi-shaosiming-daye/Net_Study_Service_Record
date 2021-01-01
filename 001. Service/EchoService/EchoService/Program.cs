47084b7c31ece2e89d6b07302225e21c02ec9abe
环镜分身 <1934036260@qq.com>
2021-01-01 13:05:00 +0800

    1 同步阻塞第一版

--- 001. Service/EchoService/EchoService/Program.cs
+++ 001. Service/EchoService/EchoService/Program.cs
@@ -6,26 +6,25 @@ namespace EchoService
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            //Socket
+            //Socket 1-1 创建一个socket
             Socket lishSocket = new Socket(AddressFamily.InterNetwork
                 , SocketType.Stream
                 , ProtocolType.Tcp);
             
             
-            //Bind
+            //Bind  1-2 绑定端口和ip
             IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
             
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddress,8888);
             
-            
-            
             lishSocket.Bind(ipEndPoint);
             
-            //Listen
+            //Listen 1-3 阻塞监听
             lishSocket.Listen(0);
             
             Console.WriteLine($"[echo服务器]启动成功!!!");
@@ -34,13 +33,13 @@ namespace EchoService
 
             while (true)
             {
-                //Accept
+                //Accept 
                 Socket acceptSocket = lishSocket.Accept();
                 Console.WriteLine($"echo服务器接收到socket连接请求");
                 
                 //Receive
                 Byte[] receiveBytes = new Byte[1024];
-                int receiveNum = acceptSocket.Receive(receiveBytes);
+                int receiveNum = acceptSocket.Receive(receiveBytes);//阻塞
                 string receiveData = System.Text.Encoding.Default.GetString(receiveBytes);
                 
                 Console.WriteLine($"服务器接收到数据!!!   => {receiveData}  小宝贝");
